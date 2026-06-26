ba3a478caff9488e4459d682f992580fa6877659
Paux14 <Ulysse.detrait14@gmail.com>
2026-06-26 10:15:49 +0200

    test: add BDD Cucumber scenarios (Gherkin)

---
+++ Exercice 13/src/test/java/bank/bdd/CucumberRunnerTest.java
@@ -0,0 +1,10 @@
+package bank.bdd;
+
+import org.junit.platform.suite.api.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = "cucumber.plugin", value = "pretty, json:target/cucumber-report.json")
+@ConfigurationParameter(key = "cucumber.glue", value = "bank.bdd.steps")
+public class CucumberRunnerTest {}
