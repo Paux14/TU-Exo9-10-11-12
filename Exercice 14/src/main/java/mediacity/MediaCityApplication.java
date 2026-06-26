59879ebc18f4e3e222b0f03219adbbd45f84c7e5
Paux14 <Ulysse.detrait14@gmail.com>
2026-06-26 11:04:06 +0200

    init: setup Maven project for MédiaCity

---
+++ Exercice 14/src/main/java/mediacity/MediaCityApplication.java
@@ -0,0 +1,7 @@
+package mediacity;
+
+public class MediaCityApplication {
+    public static void main(String[] args) {
+        System.out.println("MédiaCity - Gestion des prêts et réservations");
+    }
+}
